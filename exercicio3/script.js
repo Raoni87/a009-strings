5efb4645ac7918a6c402c656c6b0f6506c4f0420
Raoni87 <raonioliva@gmail.com>
2022-07-26 10:29:09 -0300

    resolucao de exercicios

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,5 @@
-//Crie a const para a frase aqui
+const avisoJorge = "Jorge tem uma casa verde e com portão azul, com os dizeres: BOAS VINDAS, mas não deixe o gato sair"; //Crie a const para a frase aqui
+
+console.log(avisoJorge.replace("verde", "rosa").replace("azul", "laranja"));
+console.log(avisoJorge.replace("verde", "rosa").replace("azul", "laranja").includes("verde", "laranja"));
+console.log(avisoJorge.toLowerCase("BOAS VINDAS"));
